aed36efb32ee60c38968bfa5f980ba5d4533467f
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-06 15:47:46 +0530

    Under Development 1

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,21 @@
+
+import { useFetch } from '../../hooks/useFetch'
+
 // Styles 
 import './Home.css'
 
+// Components
+import RecipeList from '../../components/RecipeList'
+
 export default function Home() {
+
+    const { data, isPending , error } = useFetch('http://localhost:8000/recipes')
+
     return (
-        <div>
-            Home
+        <div className="home" >
+            {error && <p className="error">{error}</p> }
+            {isPending && <p className="loading" >Loading...</p> }
+            {data && <RecipeList recipes={data} />}
         </div>
     )
 }
