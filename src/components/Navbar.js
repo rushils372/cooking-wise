3cc8eb11ee2d115ba68e4dedb0c5c2068bc4b917
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-07 15:23:51 +0530

    context api added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
+
 
 import './Navbar.css'
 
 import SearchBar from './SearchBar'
 
+
 export default function Navbar() {
+    const { color, changeColor } = useTheme()
+
     return (
-        <div className="navbar" >
-            <nav>
+        <div className="navbar" style={{ background: color }}>
+            <nav onClick={() => changeColor('black')}>
                 <Link to="/" className="brand" >
                     <h1>Cooking Wise</h1>
                 </Link>
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,29 @@
+import { createContext, useReducer } from "react";
+
+export const ThemeContext = createContext()
+
+const themeReducer = (state, action) => {
+    switch(action.type) {
+        case 'CHANGE_COLOR':
+            return { ...state, color: action.payload }
+        default:
+            return state
+    } 
+}
+
+export function ThemeProvider({ children }) {
+    const [state, dispatch] = useReducer(themeReducer, {
+        color: 'blue'
+    })
+
+    const changeColor = (color) => {
+        dispatch({ type: 'CHANGE_COLOR', payload: color})
+    }
+
+    return (
+        <ThemeContext.Provider value={{ ...state, changeColor}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+
+}
